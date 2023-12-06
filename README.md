85abb92426cd1a4d51852c55c9e3646c35854913
khanhbinh-ngo <khanhbinh.ngo@gmail.com>
2023-12-06 23:36:14 +0700

    update settings.json for no reason

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Embedded-exercises
- 
---
+++ src/check_sensor.c
@@ -0,0 +1,12 @@
+void setup(){
+
+}
+
+// This contains one code block to control test file 
+// check for water sensor
+
+void loop(){
+
+}  
+
+// a place that we need to be done
